eeaa91704aeb0af3aae94e0a9e03455b9f54e46f
enwelumchiamaka <enwelumchiamaka@gmail.com>
2021-07-08 17:37:17 +0100

    learning Tailwindcss

---
+++ tailwind-default.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontSize: {
+      mammoth:"8rem",
+    },
+    
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
